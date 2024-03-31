fe3ed3e446a4e82cff0956baf03e23463c1919b2
Jade Lynn Masker <donottellmetonottellyou@gmail.com>
2024-03-31 16:11:57 -0400

    remove broken plasma-browser-integration

--- os/desktop.nix
+++ os/desktop.nix
@@ -4,9 +4,13 @@
     enable = true;
     displayManager.sddm.enable = true;
     desktopManager.plasma5.enable = true;
-    # Remove xterm
+    # Remove xterm from default xserver install
     excludePackages = with pkgs; [
       xterm
     ];
   };
+  # Remove broken browser integration (it doesn't work on Chrome)
+  environment.plasma5.excludePackages = with pkgs.libsForQt5; [
+    plasma-browser-integration
+  ];
 }
