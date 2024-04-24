432df5eed3d13ae32fba8156b286b73408bd0b76
Jade Lynn Masker <donottellmetonottellyou@gmail.com>
2024-04-24 14:34:29 -0400

    add ports for expo go

--- machine/network.nix
+++ machine/network.nix
@@ -10,8 +10,9 @@
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Needed for expo go
+  networking.firewall.allowedTCPPorts = [ 8081 ];
+  networking.firewall.allowedUDPPorts = [ 8081 ];
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
 }
