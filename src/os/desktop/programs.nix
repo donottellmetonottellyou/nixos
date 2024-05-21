8459758eac6a8b7ad8663467f83edc0f8cd8d7c3
Jade Lynn Masker <donottellmetonottellyou@gmail.com>
2024-05-20 23:07:49 -0400

    update steam to allow gamescope and remote play

--- src/os/desktop/programs.nix
+++ src/os/desktop/programs.nix
@@ -2,6 +2,8 @@
   programs = {
     steam = {
       enable = true;
+      gamescopeSession.enable = true;
+      remotePlay.openFirewall = true;
       # localNetworkGameTransfers.openFirewall = true; # future config
       extraCompatPackages = with pkgs; [
         proton-ge-bin
