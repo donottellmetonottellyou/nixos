7a9779a59ba7d1a2b6aa637a9f388b70157c6622
Jade Lynn Masker <donottellmetonottellyou@gmail.com>
2024-07-11 19:32:17 -0400

    fix minor typo + add git-push-worktree

--- shell.nix
+++ shell.nix
@@ -1,12 +1,31 @@
 { pkgs ? import <nixpkgs> { } }:
-pkgs.mkShell {
-  packages = with pkgs; [
-    (writeShellScriptBin "git-apply-wortree" ''
-      git add -A &&
-        git commit
+let
+  git-apply-worktree = pkgs.writeShellScriptBin "git-apply-worktree" ''
+    git add -A &&
+      git commit
+
+    cd /etc/nixos &&
+      sudo git merge --ff-only worktree &&
+      cd /home/jadelynnmasker/Code/NixOS || {
+        echo "Failed to apply changes cleanly to /etc/nixos!"
+        echo "Was /etc/nixos dirtied?"
+        exit 1
+      }
+  '';
+  git-push-worktree = pkgs.writeShellScriptBin "git-push-worktree" ''
+    ${git-apply-worktree}/bin/git-apply-worktree &&
       cd /etc/nixos &&
-        sudo git merge --ff-only worktree &&
-        cd /home/jadelynnmasker/Code/NixOS
-    '')
+      git push &&
+      cd /home/jadelynnmasker/Code/NixOS || {
+        echo "Failed to push changes to remote!"
+        echo "Do changes in the remote need to be integrated?"
+        exit 1
+      }
+  '';
+in
+pkgs.mkShell {
+  packages = [
+    git-apply-worktree
+    git-push-worktree
   ];
 }
