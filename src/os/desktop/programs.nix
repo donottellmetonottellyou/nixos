e62ce398b462f10b3df691655d64dbe2e548b188
Jade Lynn Masker <donottellmetonottellyou@gmail.com>
2024-07-27 19:18:25 -0400

    remove gamescope from steam

--- src/os/desktop/programs.nix
+++ src/os/desktop/programs.nix
@@ -3,7 +3,6 @@
     firefox.enable = true;
     steam = {
       enable = true;
-      gamescopeSession.enable = true;
       remotePlay.openFirewall = true;
       localNetworkGameTransfers.openFirewall = true;
       extraCompatPackages = with pkgs; [
