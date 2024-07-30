10fe6b63a05a35fba089acef369ec1bb0b17beb2
Jade Lynn Masker <donottellmetonottellyou@gmail.com>
2024-07-29 20:28:08 -0400

    add syncthing to desktop services

--- src/os/desktop/services.nix
+++ src/os/desktop/services.nix
@@ -1,9 +1,40 @@
-{ lib, ... }: {
+{ ... }: {
   services = {
     # Mumble chat server
     murmur = {
       enable = true;
       openFirewall = true;
+    };
+
+    # Peer-to-peer cloud synchronization
+    syncthing = {
+      enable = true;
+      openDefaultPorts = true;
+      guiAddress = null;
+      dataDir = "/home/jadelynnmasker";
+      settings = {
+        options = {
+          limitBandwidthInLan = true;
+          maxFolderConcurrency = 1;
+          relaysEnabled = false;
+          urAccepted = 1;
+        };
+        devices = {
+          dendorian.id = "WEH2EXP-CS72MGX-Z4OBGGI-63QCS3J-2VNNMKA-NZJVWP6-UZZ5ACW-N4SRDAZ";
+        };
+        folders = {
+          Camera = {
+            path = "~/Pictures/Camera";
+            devices = [ "dendorian" ];
+            copyOwnershipFromParent = true;
+          };
+          Documents = {
+            path = "~/Documents";
+            devices = [ "dendorian" ];
+            copyOwnershipFromParent = true;
+          };
+        };
+      };
     };
   };
 }
