19deb6c35108bad6b597ad570ef3ffefda206c8a
Jade Lynn Masker <donottellmetonottellyou@gmail.com>
2024-08-05 20:36:45 -0400

    downgrade linux 6.6 -> 6.1

--- src/machine/host.nix
+++ src/machine/host.nix
@@ -1,12 +1,16 @@
-{ ... }: {
+{ pkgs, ... }: {
   # Bootloader.
-  boot.loader = {
-    systemd-boot = {
-      enable = true;
-      configurationLimit = 16;
-      editor = false;
+  boot = {
+    loader = {
+      systemd-boot = {
+        enable = true;
+        configurationLimit = 16;
+        editor = false;
+      };
+      efi.canTouchEfiVariables = true;
     };
-    efi.canTouchEfiVariables = true;
+    # Temporary fix for kernel audio issues
+    kernel = pkgs.linuxKernel.packages.linux_6_1;
   };
 
   networking.hostName = "ananda"; # Define your hostname.
