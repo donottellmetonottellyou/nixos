511496dd15ca3cdc86c0b3a2c0db0debafcd1e56
Jade Lynn Masker <donottellmetonottellyou@gmail.com>
2024-09-18 20:39:24 -0400

    change zsh theme to robbyrussell

--- src/system/programs.nix
+++ src/system/programs.nix
@@ -6,7 +6,7 @@
       autosuggestions.enable = true;
       ohMyZsh = {
         enable = true;
-        theme = "steeef";
+        theme = "robbyrussell";
       };
       interactiveShellInit = ''
         if [[ -z "$ZELLIJ" ]]; then
