01761d90f70d925bfe0986cf466530de8604a556
Jade Lynn Masker <donottellmetonottellyou@gmail.com>
2024-10-04 15:52:46 -0400

    add syncthing back

--- src/users/jadelynnmasker.nix
+++ src/users/jadelynnmasker.nix
@@ -5,6 +5,36 @@
     extraGroups = [ "networkmanager" "wheel" ];
   };
 
+  services.syncthing = {
+    enable = true;
+
+    user = "jadelynnmasker";
+    dataDir = "/home/jadelynnmasker";
+
+    openDefaultPorts = true;
+    settings = {
+      devices.dendorian.id = "WEH2EXP-CS72MGX-Z4OBGGI-63QCS3J-2VNNMKA-NZJVWP6-UZZ5ACW-N4SRDAZ";
+      folders = {
+        "Camera" = {
+          path = "${config.services.syncthing.dataDir}/Pictures/Camera";
+          devices = [ "dendorian" ];
+          versioning = {
+            type = "trashcan";
+            params.cleanoutDays = 30;
+          };
+        };
+        "Documents" = {
+          path = "${config.services.syncthing.dataDir}/Documents";
+          devices = [ "dendorian" ];
+          versioning = {
+            type = "trashcan";
+            params.cleanoutDays = 30;
+          };
+        };
+      };
+    };
+  };
+
   home-manager.users.jadelynnmasker = { pkgs, ... }: {
     home.stateVersion = config.system.stateVersion;
 
