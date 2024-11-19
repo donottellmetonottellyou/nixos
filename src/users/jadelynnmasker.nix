e10c1d4fd0856956a650fb43d7b9b40bf0391edb
Jade Lynn Masker <donottellmetonottellyou@gmail.com>
2024-11-19 14:28:01 -0500

    extract out syncthing to its own module

--- src/users/jadelynnmasker.nix
+++ src/users/jadelynnmasker.nix
@@ -6,35 +6,9 @@
     shell = pkgs.zsh;
   };
 
-  services.syncthing = {
-    enable = true;
-
-    user = "jadelynnmasker";
-    dataDir = "/home/jadelynnmasker";
-
-    openDefaultPorts = true;
-    settings = {
-      devices.dendorian.id = "XCOYGHU-ZCI7VDM-MGE4XHN-XRQGZTM-X3XHD6P-VG2XM7K-633PSJS-P6K3KA5";
-      folders = {
-        "Camera" = {
-          path = "${config.services.syncthing.dataDir}/Pictures/Camera";
-          devices = [ "dendorian" ];
-          versioning = {
-            type = "trashcan";
-            params.cleanoutDays = 30;
-          };
-        };
-        "Documents" = {
-          path = "${config.services.syncthing.dataDir}/Documents";
-          devices = [ "dendorian" ];
-          versioning = {
-            type = "trashcan";
-            params.cleanoutDays = 30;
-          };
-        };
-      };
-    };
-  };
+  imports = [
+    ./jadelynnmasker/syncthing.nix
+  ];
 
   home-manager.users.jadelynnmasker = { pkgs, ... }: {
     home.stateVersion = config.system.stateVersion;
