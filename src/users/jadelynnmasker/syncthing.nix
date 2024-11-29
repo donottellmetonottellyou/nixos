4a2dce9c65d866bf4e0a1dba978151372e7acd09
Jade Lynn Masker <donottellmetonottellyou@gmail.com>
2024-11-28 20:28:48 -0500

    make syncthing devices more flexible not overriding

--- src/users/jadelynnmasker/syncthing.nix
+++ src/users/jadelynnmasker/syncthing.nix
@@ -6,12 +6,11 @@
     dataDir = "/home/jadelynnmasker";
 
     openDefaultPorts = true;
+    overrideDevices = false;
     settings = {
-      devices.dendorian.id = "XCOYGHU-ZCI7VDM-MGE4XHN-XRQGZTM-X3XHD6P-VG2XM7K-633PSJS-P6K3KA5";
       folders = {
         "Camera" = {
           path = "${config.services.syncthing.dataDir}/Pictures/Camera";
-          devices = [ "dendorian" ];
           versioning = {
             type = "trashcan";
             params.cleanoutDays = 30;
@@ -19,7 +18,6 @@
         };
         "Documents" = {
           path = "${config.services.syncthing.dataDir}/Documents";
-          devices = [ "dendorian" ];
           versioning = {
             type = "trashcan";
             params.cleanoutDays = 30;
