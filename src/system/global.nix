17190eea149062056c3451b66f23b1eccb6b7f2a
Jade Lynn Masker <donottellmetonottellyou@gmail.com>
2025-02-04 14:55:17 -0500

    change time to en_DK.UTF-8 for ISO standard time

--- src/system/global.nix
+++ src/system/global.nix
@@ -51,7 +51,10 @@
   # ============================================================================
   # \/ TIME & REGION \/
   time.timeZone = "America/Detroit";
-  i18n.defaultLocale = "en_US.UTF-8";
+  i18n = {
+    defaultLocale = "en_US.UTF-8";
+    extraLocaleSettings.LC_TIME = "en_DK.UTF-8";
+  };
   # /\ TIME & REGION /\
   # ============================================================================
   # \/ USERS \/
