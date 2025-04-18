16eecb693e6b0e6a540a406094114abd94d4ca7c
Jade Lynn Masker <donottellmetonottellyou@gmail.com>
2025-04-18 11:17:57 -0400

    don't garbage collect, I am often w/o internet

--- src/system/global.nix
+++ src/system/global.nix
@@ -23,11 +23,6 @@
   # ============================================================================
   # \/ NIX CONFIG \/
   nix = {
-    gc = {
-      automatic = true;
-      dates = "daily";
-      options = "--delete-older-than 30d";
-    };
     optimise = {
       automatic = true;
       dates = [ "daily" ];
