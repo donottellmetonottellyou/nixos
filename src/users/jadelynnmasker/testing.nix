047a7a459f89b3b0628aad56cdecb8f15023b40c
Jade Lynn Masker <donottellmetonottellyou@gmail.com>
2025-05-14 20:14:14 -0400

    legendsviewer: add better unix directory handling

--- src/users/jadelynnmasker/testing.nix
+++ src/users/jadelynnmasker/testing.nix
@@ -4,6 +4,7 @@
     (legendsviewer-next.overrideAttrs {
       patches = [
         ./legendsviewer-always-open-browser.patch
+        ./legendsviewer-better-unix-directory-handling.patch
         ./legendsviewer-fix404.patch
       ];
     })
