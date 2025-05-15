bc989b3b9347d26020652499b28a11d73d243b67
Jade Lynn Masker <donottellmetonottellyou@gmail.com>
2025-05-14 20:13:42 -0400

    legendsviewer: rename convention

--- src/users/jadelynnmasker/testing.nix
+++ src/users/jadelynnmasker/testing.nix
@@ -3,7 +3,7 @@
   home.packages = with pkgs.donottellmetonottellyou; [
     (legendsviewer-next.overrideAttrs {
       patches = [
-        ./always-open-browser.patch
+        ./legendsviewer-always-open-browser.patch
         ./legendsviewer-fix404.patch
       ];
     })
