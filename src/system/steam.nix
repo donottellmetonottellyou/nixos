ff9c4c9a9c4776bb8d349e3add12a5275f2be313
Jade Lynn Masker <donottellmetonottellyou@gmail.com>
2025-05-30 17:40:48 -0400

    steam: disable gamescope

--- src/system/steam.nix
+++ src/system/steam.nix
@@ -2,7 +2,6 @@
 {
   programs.steam = {
     enable = true;
-    gamescopeSession.enable = true;
     remotePlay.openFirewall = true;
     localNetworkGameTransfers.openFirewall = true;
     extraCompatPackages = [
