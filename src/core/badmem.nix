be29e42f903ef53f1441e05050e7cd189663af14
Jade Lynn Masker <donottellmetonottellyou@gmail.com>
2025-11-27 18:17:44 -0500

    badmem: remove bad memory

--- src/core/badmem.nix
+++ src/core/badmem.nix
@@ -1,11 +1,11 @@
 { ... }:
 {
   boot.kernelParams = [
-    # I need new memory...
-    "memmap=0x100000$0x14f100000"
-    "memmap=0x100000$0x181d00000"
-    "memmap=0x100000$0x1e2700000"
-    "memmap=0x100000$0x2adb00000"
-    "memmap=0x100000$0x2fa700000"
+    # I got new memory ❤️ Sam!
+    # "memmap=0x100000$0x14f100000"
+    # "memmap=0x100000$0x181d00000"
+    # "memmap=0x100000$0x1e2700000"
+    # "memmap=0x100000$0x2adb00000"
+    # "memmap=0x100000$0x2fa700000"
   ];
 }
