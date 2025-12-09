b669ad220ef7b1afe6772aa414dc49a289995033
Jade Lynn Masker <donottellmetonottellyou@gmail.com>
2025-12-09 15:42:51 -0500

    pipewire: switch to 48000 sample rate

--- src/users/jadelynnmasker/pipewire.nix
+++ src/users/jadelynnmasker/pipewire.nix
@@ -8,7 +8,7 @@
       }
       {
         cmd = "load-module";
-        args = "module-simple-protocol-tcp listen=0.0.0.0 port=12345 record=true source=input.network.monitor";
+        args = "module-simple-protocol-tcp rate=48000 listen=0.0.0.0 port=12345 record=true source=input.network.monitor";
       }
     ];
   };
