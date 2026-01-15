58dd42b185aea4e5f6590aff2d11b22e712ac626
Jade Lynn Masker <donottellmetonottellyou@gmail.com>
2026-01-15 16:10:27 -0500

    gamescope: reenable for ksp compat

--- src/system/steam.nix
+++ src/system/steam.nix
@@ -2,6 +2,13 @@
 {
   programs.steam = {
     enable = true;
+    gamescopeSession = {
+      enable = true;
+      args = [
+        "--mouse-sensitivity"
+        "1.5"
+      ];
+    };
     remotePlay.openFirewall = true;
     localNetworkGameTransfers.openFirewall = true;
     extraCompatPackages = [
