bc7027f441092630b49f7a13ccde0753504d2c44
Jade Lynn Masker <donottellmetonottellyou@gmail.com>
2026-05-07 15:33:38 -0400

    testing: clean up legendsviewer-next for update

--- src/users/jadelynnmasker/testing.nix
+++ src/users/jadelynnmasker/testing.nix
@@ -1,13 +1,6 @@
 { pkgs, ... }:
 {
   home.packages = with pkgs.donottellmetonottellyou; [
-    (legendsviewer-next.override {
-      patches = [
-        ./patches/legendsviewer-always-open-browser.patch
-        ./patches/legendsviewer-better-unix-directory-handling.patch
-        ./patches/legendsviewer-favorites.patch
-        ./patches/legendsviewer-fix404.patch
-      ];
-    })
+    legendsviewer-next
   ];
 }
